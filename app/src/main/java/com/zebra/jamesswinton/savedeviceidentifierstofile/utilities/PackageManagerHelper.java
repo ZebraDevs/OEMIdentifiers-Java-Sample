54ed52aed5c62a6391452a5bef6242fb60b8311c
JS6845 <JS6845@zebra.com>
2020-12-01 14:10:26 +0000

    Added Android Oreo (8.1) support

--- app/src/main/java/com/zebra/jamesswinton/savedeviceidentifierstofile/utilities/PackageManagerHelper.java
+++ app/src/main/java/com/zebra/jamesswinton/savedeviceidentifierstofile/utilities/PackageManagerHelper.java
@@ -5,22 +5,13 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.content.pm.SigningInfo;
+import android.os.Build;
+import android.util.Base64;
 
 import org.apache.commons.codec.DecoderException;
-import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.Hex;
 
 public class PackageManagerHelper {
-    @SuppressWarnings("deprecation")
-    @SuppressLint("PackageManagerGetSignatures")
-    public static Signature[] getSigningCertificateHex(Context cx)
-            throws PackageManager.NameNotFoundException {
-        Signature[] sigs;
-        SigningInfo signingInfo;
-        signingInfo = cx.getPackageManager().getPackageInfo(cx.getPackageName(), PackageManager.GET_SIGNING_CERTIFICATES).signingInfo;
-        sigs = signingInfo.getApkContentsSigners();
-        return sigs;
-    }
 
     public static String getSigningCertBase64(Context cx) throws PackageManager.NameNotFoundException, DecoderException {
         //convert String to char array (1st step)
@@ -30,6 +21,21 @@ public class PackageManagerHelper {
         byte[] decodedHex = Hex.decodeHex(charArray);
 
         // The String decoded to Base64 (3rd step)
-        return Base64.encodeBase64String(decodedHex);
+        // return Base64.encodeBase64String(decodedHex); -> Throws error on Android 8
+        return android.util.Base64.encodeToString(decodedHex, Base64.NO_WRAP);
+    }
+
+    @SuppressLint("PackageManagerGetSignatures")
+    public static Signature[] getSigningCertificateHex(Context cx)
+            throws PackageManager.NameNotFoundException {
+        Signature[] sigs;
+        SigningInfo signingInfo;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            signingInfo = cx.getPackageManager().getPackageInfo(cx.getPackageName(), PackageManager.GET_SIGNING_CERTIFICATES).signingInfo;
+            sigs = signingInfo.getApkContentsSigners();
+        } else {
+            sigs = cx.getPackageManager().getPackageInfo(cx.getPackageName(), PackageManager.GET_SIGNATURES).signatures;
+        }
+        return sigs;
     }
 }
